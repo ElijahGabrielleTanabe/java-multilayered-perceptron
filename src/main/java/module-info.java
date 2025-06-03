dc0bdcd525519b8c9a26a9817880011de47da84f
ElijahGabrielleTanabe <elijahgabriellet@gmail.com>
2025-06-03 01:14:45 -0500

    release: v1.0.2 image

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.github.elijahgabrielletanabe {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-    opens com.github.elijahgabrielletanabe.Controllers to javafx.fxml;
-    exports com.github.elijahgabrielletanabe;
-    exports com.github.elijahgabrielletanabe.Controllers;
-}
